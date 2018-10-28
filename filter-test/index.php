916ad7b1f04a1ca6271cc12e26091258970dafac
lmitchell524 <lmitchell524@gmail.com>
2018-10-28 15:37:21 -0700

    created displayproducts and build products functions as well as shirt filter

--- filter-test/index.php
+++ filter-test/index.php
@@ -4,14 +4,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="style.css">
-      <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
-      <script src="main.js"></script>
-      <title>Lori's Filter Test</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <script>
+	   var productsList = <?php echo json_encode(getProductCollection()); ?>;
+    </script>
+    <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+    <script src="main.js"></script>
+    <title>Lori's Filter Test</title>
   </head>
   <body>
       <div id='filters'>
--- filter-test/main.js
+++ filter-test/main.js
@@ -2,31 +2,51 @@
 $(document).ready(initializeApp);
 
 function initializeApp(){
-    buildProductLayout();
-    // addClickHandlersToElements();
-    // getDataFromServer();
+    displayAllProducts();
+    addClickHandlersToElements();
 }
 
-var product = {
-        id: 1,
-        name: 'White T-Shirt #1',
-        color: 'white',
-        category: 'shirts',
-        price: 12.00,
-        image: 'image.png'
-};
-
-function buildProductLayout() {
-    var newProduct = $('<div>');
-    var name = $('<p>').text(product.name);
-    var category = $('<p>').text('Category: ' + product.category);
-    var color = $('<p>').text('Color: ' + product.color);
-    var price = $('<p>').text('Price: $' + product.price);
-
-    $('#main').append(newProduct);
-    (newProduct).append(name, category,color, price);
+function addClickHandlersToElements(){
+    $('#shirts').on('click', handleShirtFilter);
+    $('#pants').on('click', handlePantsFilter);
+    $('#headwear').on('click', handleHeadwearFilter);
 }
 
-function filterByCategory() {
-    
+//display all products on page load
+function displayAllProducts(){
+    for(var i = 0; i < productsList.length; i++){
+        var productToBuild = productsList[i];
+        buildProduct(productToBuild);
+    }
+}
+
+//build out the product with it's attributes
+function buildProduct(productToBuild) {
+        var newProduct = $('<div>').addClass('product-container').attr('id', productToBuild.id).attr('data-category', productToBuild.category);
+        var name = $('<p>').text(productToBuild.name).addClass('name');
+        var category = $('<p>').text('Category: ' + productToBuild.category);
+        var color = $('<p>').text('Color: ' + productToBuild.color);
+        var price = $('<p>').text('Price: $' + productToBuild.price);
+
+        $('#main').append(newProduct);
+        (newProduct).append(name, category,color, price);
+}
+
+function handleShirtFilter() {
+    if(($('#shirts')[0].checked)){
+        $('.product-container').hide();
+        for(var i = 0; i < ($('#shirts')[0].checked); i++){
+            $('.product-container[data-category=' + this.id + ']').show();
+        }
+    } else {
+        $('.product-container').show();
+    }
+}
+
+function handlePantsFilter() {
+    console.log('pants clickhandler working');
+}
+
+function handleHeadwearFilter() {
+    console.log('headwear clickhandler working');
 }
